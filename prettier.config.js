28fb82306e2c59e2a149797441800c1af62c9a7f
konnorrogers <konnor5456@gmail.com>
2024-12-18 10:57:06 -0500

    repro

--- prettier.config.js
+++ prettier.config.js
@@ -1,14 +1,14 @@
 /** @type {import("prettier").Config} */
 const config = {
   plugins: ["prettier-plugin-jinja-template"],
-  overrides: [
-    {
-      files: ["*.njk"],
-      options: {
-        parser: "jinja-template",
-      },
-    },
-  ],
+  // overrides: [
+  //   {
+  //     files: ["*.njk"],
+  //     options: {
+  //       parser: "jinja-template",
+  //     },
+  //   },
+  // ],
 };
 
 export default config;
